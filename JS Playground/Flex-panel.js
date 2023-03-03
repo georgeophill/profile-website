78069b79d8664ae235de5f7c451a3029509a4647
George Hill <georgeophill@outlook.com>
2023-03-03 10:40:32 +0000

    Final touches

--- JS Playground/Flex-panel.js
+++ JS Playground/Flex-panel.js
@@ -3,9 +3,7 @@ const panels = document.querySelectorAll('.panel');
 function toggleOpen() {
   this.classList.toggle('open');
 }
-
 function toggleActive(e) {
-  console.log(e.propertyName);
   if(e.propertyName.includes("flex")) {
     this.classList.toggle('open-active');
   }
