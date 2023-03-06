11ee2c69176a3042269c97a15789ce0c3b90f645
George Hill <georgeophill@outlook.com>
2023-03-06 11:50:29 +0000

    eventlistener

--- JS Playground/06 - Type Ahead/type-ahead.js
+++ JS Playground/06 - Type Ahead/type-ahead.js
@@ -4,4 +4,23 @@ let cities = [];
 
 const prom = fetch(endpoint)
       .then(blob => blob.json())
-      .then(data => cities.push(data));
+      .then(data => cities.push(...data));
+
+      function findMatches(wordToMatch, cities) {
+        return cities.filter(place => {
+          // here we need to figure out if the city or state matches what was searched
+          let regex = new RegExp(wordToMatch, 'gi'); // "gi" -  "g"= global search  "i" = case insensitive
+          return place.city.match(regex) || place.state.match(regex);
+        });
+      }
+
+      function displayMatches() {
+        const matchArray = findMatches(this.value.cities)
+        console.log(matchArray)
+      }
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
