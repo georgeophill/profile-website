057ed007338dbb4b273596d17b04dbf420abd2a7
George Hill <georgeophill@outlook.com>
2023-03-07 10:20:08 +0000

    .every arrow function

--- JS Playground/07-Array-Cardio/array-cardio-d2.js
+++ JS Playground/07-Array-Cardio/array-cardio-d2.js
@@ -37,6 +37,10 @@ const isMan2 = people.some(person => ((new Date()).getFullYear()) - person.year 
 
 // Array.prototype.every() // is everyone 19 or older?
 
+const allAdults = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+console.log(allAdults)
+
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
