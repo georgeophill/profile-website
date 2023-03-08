0463c86377c782b105cce94348f5c9a61dbc12f3
George Hill <georgeophill@outlook.com>
2023-03-08 13:16:49 +0000

    function complete

--- JS Playground/08-Fun with HTML/canvas.js
+++ JS Playground/08-Fun with HTML/canvas.js
@@ -4,25 +4,34 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
-ctx.lineCsp = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 20;
+
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
 
 function draw(e) {
   if (!isDrawing) return // stop from running when they are not moused down
   console.log(e)
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
   ctx.beginPath();
   //start from
   ctx.moveTo(lastX, lastY);
   // go to
   ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke()
-  [lastX, lastY] = [e.offsetX, e.offsetY]
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  hue++
 }
 
+canvas.addEventListener('mousedown', (e) => {
+isDrawing = true;
+[lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true );
 canvas.addEventListener('mouseup', () => isDrawing = false );
 canvas.addEventListener('mouseout', () => isDrawing = false );
