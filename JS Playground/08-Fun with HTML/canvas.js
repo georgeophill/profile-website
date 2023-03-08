53bb4c6700fdc37dfda467203155112d849cc600
George Hill <georgeophill@outlook.com>
2023-03-08 12:55:19 +0000

    draw function

--- JS Playground/08-Fun with HTML/canvas.js
+++ JS Playground/08-Fun with HTML/canvas.js
@@ -1,1 +1,28 @@
-console.log("it works")
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCsp = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e) {
+  if (!isDrawing) return // stop from running when they are not moused down
+  console.log(e)
+  ctx.beginPath();
+  //start from
+  ctx.moveTo(lastX, lastY);
+  // go to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke()
+  [lastX, lastY] = [e.offsetX, e.offsetY]
+}
+
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousedown', () => isDrawing = true );
+canvas.addEventListener('mouseup', () => isDrawing = false );
+canvas.addEventListener('mouseout', () => isDrawing = false );
